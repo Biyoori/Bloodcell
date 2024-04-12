03ff66d1c7bda81a3d096e8a7b109ca487733dea
Biyori <Grzany10@gmail.com>
2024-04-12 03:01:58 +0200

    First release of Bloodcell

---
+++ main.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.locals import *
+from player import Player
+
+winWidth, winHeight = 740, 740
+fps = 60
+
+cellSize = 64
+
+
+colors = {"black": (0, 0, 0),
+          "white": (255, 255, 255),
+          "gray": (50, 50, 50),
+          "red": (200, 30, 30),
+          "darkblue": (24, 20, 37)}
+
+grid = [[1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,1,1,1,1,1,1,1,1],
+        [1,0,0,0,0,0,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,2,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1]]
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((winWidth, winHeight))
+    clock = pygame.time.Clock()
+    pygame.display.set_caption("2D Puzzle")
+
+    tile = pygame.image.load("Data\Tiles\\tile2.png").convert_alpha()
+    playerBlock = pygame.image.load("Data\Tiles\player-block.png").convert_alpha()
+    player = Player(8*cellSize, 2*cellSize, cellSize)
+
+    run = True
+    while run:
+        clock.tick(fps)
+        pygame.display.update()
+
+        screen.fill("black")
+        
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == 1:
+                    screen.blit(pygame.transform.scale(tile, (cellSize, cellSize)), (j* cellSize, i*cellSize))
+                elif cell == 2:
+                    screen.blit(pygame.transform.scale(playerBlock, (cellSize, cellSize)), (j* cellSize, i*cellSize)) 
+                else:
+                    pygame.draw.rect(screen, colors["darkblue"], (j * cellSize, i * cellSize, cellSize, cellSize))
+
+        player.draw(screen)
+        player.update()
+        player.moveUpdate(grid)
+
+        #pygame.draw.rect(screen, colors["red"], player.hitBox)
+
+
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                run = False
+            elif e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_a:
+                    player.move(-1, grid)
+                elif e.key == pygame.K_d:
+                    player.move(1, grid)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.move(-1, grid)
+        if keys[pygame.K_RIGHT]:
+            player.move(1, grid)
+            
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,105 @@
+import pygame
+
+class Player:
+    def __init__(self, posX, posY, cellSize) -> None:
+        self.posX = posX
+        self.posY = posY
+        self.idleSprite = pygame.image.load("Data\Sprites\Player\player-idle.png").convert_alpha()
+        self.moveSprite = pygame.image.load("Data\Sprites\Player\player-move.png").convert_alpha()
+        self.fallSprite = pygame.image.load("Data\Sprites\Player\player-fall.png").convert_alpha()
+        self.currentFrame = 0
+        self.idleFrames = self.loadFrames(self.idleSprite, 0, 6)
+        self.moveFrames = self.loadFrames(self.moveSprite, 0, 8)
+        self.fallFrames = self.loadFrames(self.fallSprite, 0, 3)
+        self.animationSpeed = 0.1
+        self.cellSize = cellSize
+        self.speed = 3
+        self.targetX = self.posX
+        self.targetY = self.posY
+        self.isMoving = False
+        self.isFalling = False
+        self.prevAnim = self.idleFrames
+        self.activeDirection = 1
+        self.activeAnim = self.animChange(self.idleFrames, self.moveFrames, self.fallFrames)
+        self.hitBox = pygame.Rect(self.posX + self.cellSize/4, self.posY + self.cellSize/4, *pygame.Surface.get_size(self.activeAnim[0]))
+
+    def loadFrames(self, spritesheet, startIndex, endIndex):
+        frameWidth = 32
+        frameHeight = 32
+        frames = []
+        for i in range(startIndex * frameWidth, endIndex * frameWidth, frameWidth):
+            frame = spritesheet.subsurface((i, 0, frameWidth, frameHeight))
+            frames.append(frame)
+        return frames
+    
+    def draw(self, surface):
+        frames = self.animChange(self.idleFrames, self.moveFrames, self.fallFrames)
+        if self.isAnimChanged(frames):
+            self.currentFrame = 0
+        currentFrame = frames[int(self.currentFrame)]
+        if self.activeDirection is -1:
+            currentFrame = pygame.transform.flip(currentFrame, True, False)
+        surface.blit(pygame.transform.scale(currentFrame, (64, 64), ), (self.posX, self.posY))
+
+    def update(self):
+        frames = self.animChange(self.idleFrames, self.moveFrames, self.fallFrames)
+        if self.isAnimChanged(frames):
+            self.currentFrame = 0
+        self.currentFrame += self.animationSpeed
+        if self.currentFrame >= len(frames):
+            self.currentFrame = 0
+
+    def animChange(self, idleAnim, moveAnim, fallAnim):
+        if self.isMoving:
+            frames = moveAnim
+            self.animationSpeed = 0.2
+        elif self.isFalling:
+            frames = fallAnim
+            self.animationSpeed = 0.1
+        else:
+            frames = idleAnim
+            self.animationSpeed = 0.1
+        return frames
+
+    def isAnimChanged(self, anim):
+        if anim != self.prevAnim:
+            self.prevAnim = anim
+            return True
+        else:
+            return False
+
+    def move(self, direction, grid):
+            if self.posX == self.targetX and not self.isFalling:
+                newTargetX = self.posX + direction * self.cellSize
+                if grid[self.posY//self.cellSize][newTargetX//self.cellSize] != 1:
+                    self.targetX = newTargetX
+                    self.activeDirection = direction
+
+    def moveUpdate(self, grid):
+        if self.posX == self.targetX:
+            self.isMoving = False
+            
+        else:
+            self.isMoving = True
+            dx = self.targetX - self.posX
+            direction = 1 if dx > 0 else -1
+            step = min(abs(dx), self.speed)
+            self.posX += direction * step
+
+        self.hitBox = pygame.Rect(self.posX + self.cellSize/4, self.posY + self.cellSize/4, *pygame.Surface.get_size(self.activeAnim[0]))
+        newTargetY = self.posY + self.cellSize
+        targetGridX = int(self.posX//self.cellSize)
+               
+        if self.posY < (len(grid)-1)*self.cellSize and grid[newTargetY//self.cellSize][targetGridX] != 1:
+            self.isFalling = True
+            self.targetY = newTargetY
+            if not self.isMoving:
+                self.posY += self.speed
+        else:
+            self.isFalling = False
+
+    def isColliding(self, collisionArea):
+        if self.hitBox.colliderect(collisionArea):
+            return True
+        else:
+            return False
